1da8a7ef70a7eada9d12a684bf90071526aaba06
tomturner14 <tomfrom1994@gmail.com>
2024-07-18 21:24:19 +0900

    delete Fullcalendar

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,9 @@
 "use client"
 import Image from "next/image";
 import styles from "./page.module.css";
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import interactionPlugin, { Draggable, DropArg } from '@fullcalendar/interaction';
-import timeGridPlugin from '@fullcalendar/timegrid';
 import { Fragment, useEffect, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { CheckIcon, ExclamationTriangleIcon } from '@heroicons/react/20/solid';
-import { EventSourceInput } from '@fullcalendar/core/index.js';
 import PropTypes from "prop-types"
 import "./globals.css";
 
@@ -50,37 +45,7 @@ export default function Home() {
         </div>
       </div>
       <div className="calendar">
-        <>
-          <main className="main-content">
-            <div className="calendar-grid">
-              <div className="calendar">
-                <FullCalendar
-                  plugins={[
-                    dayGridPlugin,
-                    interactionPlugin,
-                    timeGridPlugin
-                  ]}
-                  headerToolbar={{
-                    left: 'prev, next',
-                    center: 'title'
-                  }}
-                  contentHeight="auto"
-                  contentWidth="auto"
 
-                />
-              </div>
-            </div>
-          </main>
-        </>
-      </div>
-      <div>
-        <input className="date-input"></input>
-      </div>
-      <div>
-        <input className="memo-input"></input>
-      </div>
-      <div className="generate">
-        <button className="generate-event">出欠表作成</button>
       </div>
     </div>
   )
