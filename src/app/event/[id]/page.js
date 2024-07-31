26d52f383aeaabfc55b2e34451ad9a79cd43b6bf
tomturner14 <tomfrom1994@gmail.com>
2024-07-31 17:59:47 +0900

    eventpages編集

--- src/app/event/[id]/page.js
+++ src/app/event/[id]/page.js
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client";
+
+import { usePathname } from "next/navigation";
 
 const EventPage = () => {
+  const pathname = usePathname();
+
+  const data = [
+    { title: "イベントタイトル", date: "2024-07-30" },
+    { title: "イベントタイトル", date: "2024-07-30" },
+    { title: "イベントタイトル", date: "2024-07-30" },
+  ];
+
   return (
-    <div>イベントページ</div>
-  )
-}
+    <div>
+      <h1>イベントページ</h1>
+      <p>{pathname}</p>
+    </div>
+  );
+};
 
 export default EventPage
