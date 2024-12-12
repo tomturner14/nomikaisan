d522ba151d487c6727cd05664d694acfc304d24a
tomturner14 <tomfrom1994@gmail.com>
2024-12-12 20:59:35 +0900

    URL作成箇所

--- src/app/complete-page/page.js
+++ src/app/complete-page/page.js
@@ -1,23 +1,33 @@
 // src/app/complete-page/page.js
+"use client";
 
-import React from 'react';
+import { useSearchParams} from 'next/navigation';
 import styles from './page.module.css';
 import Link from 'next/link';
 
 const CompletePage = () => {
   const searchParams = useSearchParams();
-  const eventId = searchParams  
+  const eventId = searchParams.get('event_id');
+  const eventUrl = `https://nomikaisan.com/event/${eventId}`;
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>イベント作成完了</h1>
-      <p className={styles.description}>参加者へ下記URLを共有し出欠を入力してもらいましょう！</p>
+      <p className={styles.description}>
+        参加者へ下記URLを共有し出欠を入力してもらいましょう！
+      </p>
       <div className={styles.inputContainer}>
-        <input type="text" value="https://nomikainittei.com" readOnly className={styles.input}/>
+        <input
+          type="text"
+          value={eventUrl}
+          readOnly
+          className={styles.input}
+        />
       </div>
       <div className={styles.linkContainer}>
-        <h2 className={styles.link}>
-          <Link href="/event/111">イベントページへ</Link>
-        </h2>
+        <Link href={`/event/${eventId}`} className={styles.link}>
+          イベントページへ
+        </Link>
       </div>
     </div>
   );
